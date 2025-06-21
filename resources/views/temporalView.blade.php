29a65f63a08f29c05b9219d9160ad7722f426436
Codeddy <buenosotoe@gmail.com>
2025-06-21 04:22:16 +0200

    Logo añadido.

--- resources/views/temporalView.blade.php
+++ resources/views/temporalView.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LunarBeat</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('storage/Logo.png') }}">
     @vite(['resources/css/temporalView.css'])
 </head>
 
